287b0a2deaca0efe4710d63822d23aa2ab4157bb
nicomiguelino <nicomiguelino2014@gmail.com>
2024-06-30 15:08:14 -0700

    fix: disable cursor

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
+  app.setOverrideCursor(Qt::BlankCursor);
 
-  QLabel label("Hello, World!");
+  QLabel label("Lorem ipsum dolor sit amet.");
   label.setAlignment(Qt::AlignCenter);
   label.setStyleSheet("font-size: 50px");
   label.show();
